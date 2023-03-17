3f64b3e7d8af2e6831524800d649870b7c45aece
Chaitali2209 <chaitalipadalkar.it@bvcoenm.edu.in>
2023-03-17 19:53:14 +0530

    Homepage UI Improvements

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 // import 'animate.css';
+import home from "../assets/Home.png"
 import TrackVisibility from 'react-on-screen';
 import logo from '../assets/img/logo.png'
 export const Banner = () => {
@@ -59,10 +60,10 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to Udyogini - </span>
+                <span className="tagline">Welcome to Udyogini - Learn || Grow || Connect </span>
                 <h1>{`Women Entrepreneurs in rural area?`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Do they get a good platform?", "How to grow your business?", "What can be done to upscale their business?" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Women entrepreneurship has been recognized as an important source of economic growth. Women entrepreneurs create new jobs for themselves and others and also provide society with different solutions to management, organization and business problems. However, they still represent a minority of all entrepreneurs.  </p>
-                  <p>Women entrepreneurs often face gender-based barriers to starting and growing their businesses, like discriminatory property, matrimonial and inheritance laws and/or cultural practices; lack of access to formal finance mechanisms; limited mobility and access to information and networks, etc. Women’s entrepreneurship can make a particularly strong contribution to the economic well-being of the family and communities, poverty reduction and women’s empowerment, thus contributing to the Millennium development goals. Thus, governments across the world as well as various developmental organizations are actively undertaking promotion of women entrepreneurs through various schemes, incentives.</p>
+                  <p>Women entrepreneurs often face gender-based barriers to starting and growing their businesses, like discriminatory property, matrimonial and inheritance laws and/or cultural practices; lack of access to formal finance mechanisms; limited mobility and access to information and networks, etc.</p>
                   <button onClick={() => console.log('connect')} to="#contact">Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
@@ -72,7 +73,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={logo} alt="Header Img"/>
+                  <img src={home} alt="Header Img"/>
                 </div>}
             </TrackVisibility>
           </Col>
--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -4,14 +4,16 @@ import { Col, Row } from "react-bootstrap";
 import image1 from '../assets/img/Upasana Taku.png';
 import image2 from '../assets/img/Aditi Gupta.png'
 import image3 from '../assets/img/Divya Gokulnath.png'
+import home from "../assets/Home.png"
 
 export const Newsletter = ({ status }) => {
   return (
     <Col lg={12}>
       <div className="newsletter-bx wow slideInUp">
+            {/* <img src={home} alt="" className="newsletter-icon"/> */}
         <Row>
           <Col lg={12} md={6} xl={5}>
-            <h3>Udyogini's Women Enterpreneur</h3>
+            <h3>Udyogini's <br /> Women Enterpreneur</h3>
             <br></br>
             <h6>PCOS Management is more than just a number on a scale. Sakhi works with leading experts in the industry who care as much about your
               path to PCOS reversal as you do.</h6>
